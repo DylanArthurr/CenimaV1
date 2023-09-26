bfec83b35d71e1b90293aa80396f293e5a4f686b
Dylan Arthur <AQ232935@aquinas.ac.uk>
2023-09-26 13:40:57 +0100

    Refactor whole code

--- CenimaV1/Program.cs
+++ CenimaV1/Program.cs
@@ -4,43 +4,78 @@ using System.IO;
 
 namespace CinemaTicketBooking
 {
+ 
+
     class Program
     {
+        static List<Movie> Movies= new List<Movie>();
+
         static void Main(string[] args)
         {
-            TicketHandling TH = new TicketHandling();   
-            
+            loadUpMovies();
+            welcomeMenu();
+           Movie MovieChosen = movieChoice();
 
-            while (true)
+
+        }
+
+        public static void loadUpMovies()
+        { 
+            Movies.Add(new Movie("Rush", "15"));
+            Movies.Add(new Movie("How I Live Now", "15"));
+            Movies.Add(new Movie("Thor: The Dark World", "12"));
+            Movies.Add(new Movie("Filth", "18"));
+            Movies.Add(new Movie("Planes", "u"));
+          
+
+        }
+        public static Movie movieChoice()
+        {
+            Console.WriteLine("Choose Movie You Would Like To See");
+            int m = 1;
+            foreach (Movie movie in Movies)
             {
-                Menue();
-
-                MenueChoice(TH);
+                Console.WriteLine(m+": "+movie.Title + " (" + movie.Rating + ") ");
+                m++;
             }
-        }
+            int userMovieChoice = Convert.ToInt32(Console.ReadLine());
 
-        public void SellTickets(TicketHandling TH)
+            if (userMovieChoice <1 || userMovieChoice>Movies.Count)
+            {
+                Console.WriteLine("This Movie Doesnt Exitst");
+            }
+            return Movies[userMovieChoice - 1];
+        }
+        public static void welcomeMenu()
         {
-
-            TH.SellTickets();
-
+            Console.WriteLine("Welcome to Aquinas Multiplex");
+            Console.WriteLine("We are presently showing:");
+           
         }
-        private static void MenueChoice(TicketHandling TH)
+        public static int userAge(string Rating)
         {
-            TH.TicketChoice();
-        }
+            Console.WriteLine("Please Enter Your Age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            //Rating = Convert.ToInt32();
 
-        private static void Menue()
+            //if (age < Rating)
+            //{
+            //    Console.WriteLine("You Are Too Young");
+            //}
+            return age;
+        }
+    }
+    class Movie
+    {
+        public string Title = "";
+        public string Rating = "";
+        public int Tickets = 0;
+
+        public Movie(string T, string R)
         {
-            Console.WriteLine("Cinema Ticket Booking System");
-            Console.WriteLine("1. Sell Tickets");
-            Console.WriteLine("2. View Ticket Sales");
-            Console.WriteLine("3. Exit");
-            Console.Write("Select an option: ");
+            Title = T;
+            Rating = R;
+            Tickets = 0;
         }
-
-        
-
-
     }
 }
